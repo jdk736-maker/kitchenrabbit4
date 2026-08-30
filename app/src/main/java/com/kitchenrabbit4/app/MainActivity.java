9818492ff227ef3ae0d27070f7842f8e2f503e29
jdk736-maker <jdk736@gmail.com>
2026-08-30 10:17:30 +0200

    Create MainActivity.java

---
+++ app/src/main/java/com/kitchenrabbit4/app/MainActivity.java
@@ -0,0 +1,32 @@
+package com.kitchenrabbit4.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(android.R.id.content).getRootView();
+        super.onBackPressed();
+    }
+}
